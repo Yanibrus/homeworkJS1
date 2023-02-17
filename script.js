7d4e3ffc195a38b6f48db0f663c3766e63d33cd4
dududu <dudu@gmail.com>
2023-02-17 13:51:47 +0500

    first

---
+++ script.js
@@ -0,0 +1,47 @@
+// console.log(1);
+// console.log('string');
+// console.log(false);
+// console.log(true);
+
+// var name = 'Яна'
+// var age = 28
+// var boolean = true
+// var und
+
+// // console.log(name);
+// // console.log(age);
+// // console.log(boolean);
+// // console.log(und);
+
+// console.log('Я ' + name + ' мне ' + age + ' лет! ');
+// var allInfo = 'Я ' + name + ' мне ' + age + ' лет! '
+
+// console.log(allInfo);
+
+// console.log(6 + 3);
+// console.log(6 - 3);
+// console.log(6 * 3);
+// console.log(10 / 3);
+// console.log(10 % 3);
+
+
+// console.log('6 + 3 = ' + (6 + 3));
+// console.log('6 - 3 = ' + (6 - 3));
+// console.log('6 * 3 = ' + (6 * 3));
+// console.log('6 / 3 = ' + (6 / 3));
+// console.log('6 % 5 = ' + (6 % 5));
+
+// var str = prompt('Введите своё имя')
+// console.log(str + str);
+
+
+// var num = +prompt('Введите число')
+// console.log(num + num);
+
+var x = +prompt('Введите первое число')
+var y = +prompt('Введите второе число')
+var z = +prompt('Введите третье число')
+
+console.log(x + y + z);
+alert ('Результат: ' + (x + y + z) / 3)
+
